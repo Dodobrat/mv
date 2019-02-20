5d47a450080d5db4d41820f2cbc035fc0322c03c
Deyan Bozhilov <dodobrat98@gmail.com>
2019-02-20 12:23:44 +0200

    REMOVED TYPES module, creating commit for final stable version, starting nested testing

--- app/Modules/Index/Resources/Views/front/boxes/project.blade.php
+++ app/Modules/Index/Resources/Views/front/boxes/project.blade.php
@@ -1,63 +1,63 @@
-@if(!empty($project))
+{{--@if(!empty($project))--}}
 
-<div class="custom-modal-content">
-    <div class="custom-modal-header">
-        <button class="close"
-        onclick="closeModal()">
-            &#x276C;
-        </button>
-        <h3>{{ $project->title }}</h3>
-    </div>
+{{--<div class="custom-modal-content">--}}
+    {{--<div class="custom-modal-header">--}}
+        {{--<button class="close"--}}
+        {{--onclick="closeModal()">--}}
+            {{--&#x276C;--}}
+        {{--</button>--}}
+        {{--<h3>{{ $project->title }}</h3>--}}
+    {{--</div>--}}
 
-    <div class="row justify-content-center align-items-center px-0 px-sm-0 px-md-0 px-lg-3">
-        <div class="col-12">
+    {{--<div class="row justify-content-center align-items-center px-0 px-sm-0 px-md-0 px-lg-3">--}}
+        {{--<div class="col-12">--}}
 
-            <div class="tab-content" id="project-view-container">
+            {{--<div class="tab-content" id="project-view-container">--}}
 
-                @foreach($project->media as $media)
+                {{--@foreach($project->media as $media)--}}
 
-                    <div class="tab-pane fade {{ $loop->first ? 'show active' : '' }} project-view" id="pills-{{ $media->id }}" role="tabpanel" aria-labelledby="pills-{{ $media->id }}-tab">
-                        @if(!empty($media))
-                            <img src="{{ $media->getPublicPath() }}">
-                        @else
-                            <img src="https://via.placeholder.com/300C/O https://placeholder.com/" alt="">
-                        @endif
+                    {{--<div class="tab-pane fade {{ $loop->first ? 'show active' : '' }} project-view" id="pills-{{ $media->id }}" role="tabpanel" aria-labelledby="pills-{{ $media->id }}-tab">--}}
+                        {{--@if(!empty($media))--}}
+                            {{--<img src="{{ $media->getPublicPath() }}">--}}
+                        {{--@else--}}
+                            {{--<img src="https://via.placeholder.com/300C/O https://placeholder.com/" alt="">--}}
+                        {{--@endif--}}
 
-                    </div>
+                    {{--</div>--}}
 
-                @endforeach
-            </div>
+                {{--@endforeach--}}
+            {{--</div>--}}
 
-            <div class="container">
+            {{--<div class="container">--}}
 
-                <ul class="nav nav-pills justify-content-lg-center" id="project-view-pills" role="tablist">
+                {{--<ul class="nav nav-pills justify-content-lg-center" id="project-view-pills" role="tablist">--}}
 
-                    @foreach($project->media as $thumbs)
-                        <li class="nav-item">
-                            <a class="nav-link {{ $loop->first ? 'active' : '' }}"
-                               id="pills-{{ $thumbs->id }}-tab"
-                               data-toggle="pill"
-                               href="#pills-{{ $thumbs->id }}"
-                               role="tab"
-                               aria-controls="pills-{{ $thumbs->id }}"
-                               aria-selected="true">
-                                @if(!empty($thumbs))
-                                    <img src="{{ $thumbs->getPublicPath() }}">
-                                @else
-                                    <img src="https://via.placeholder.com/300C/O https://placeholder.com/" alt="">
-                                @endif
-                            </a>
-                        </li>
-                    @endforeach
+                    {{--@foreach($project->media as $thumbs)--}}
+                        {{--<li class="nav-item">--}}
+                            {{--<a class="nav-link {{ $loop->first ? 'active' : '' }}"--}}
+                               {{--id="pills-{{ $thumbs->id }}-tab"--}}
+                               {{--data-toggle="pill"--}}
+                               {{--href="#pills-{{ $thumbs->id }}"--}}
+                               {{--role="tab"--}}
+                               {{--aria-controls="pills-{{ $thumbs->id }}"--}}
+                               {{--aria-selected="true">--}}
+                                {{--@if(!empty($thumbs))--}}
+                                    {{--<img src="{{ $thumbs->getPublicPath() }}">--}}
+                                {{--@else--}}
+                                    {{--<img src="https://via.placeholder.com/300C/O https://placeholder.com/" alt="">--}}
+                                {{--@endif--}}
+                            {{--</a>--}}
+                        {{--</li>--}}
+                    {{--@endforeach--}}
 
-                </ul>
+                {{--</ul>--}}
 
-            </div>
+            {{--</div>--}}
 
 
-        </div>
-    </div>
+        {{--</div>--}}
+    {{--</div>--}}
 
-</div>
+{{--</div>--}}
 
-    @endif
+    {{--@endif--}}
--- app/Modules/Index/Resources/Views/front/index.blade.php
+++ app/Modules/Index/Resources/Views/front/index.blade.php
@@ -1,64 +1,64 @@
 @extends('layouts.app')
 @section('content')
 
-    <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-        @foreach($types as $type)
-        <li class="nav-item">
-            <a class="nav-link {{ $loop->first ? 'active' : '' }}"
-               id="pills-{{ $type->slug }}-tab"
-               data-toggle="pill"
-               href="#pills-{{ $type->slug }}"
-               role="tab"
-               aria-controls="pills-{{ $type->slug }}"
-               aria-selected="true"
-               onclick="">
-                {{ $type->title }}
-            </a>
-        </li>
-        @endforeach
-    </ul>
+    {{--<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">--}}
+        {{--@foreach($types as $type)--}}
+        {{--<li class="nav-item">--}}
+            {{--<a class="nav-link {{ $loop->first ? 'active' : '' }}"--}}
+               {{--id="pills-{{ $type->slug }}-tab"--}}
+               {{--data-toggle="pill"--}}
+               {{--href="#pills-{{ $type->slug }}"--}}
+               {{--role="tab"--}}
+               {{--aria-controls="pills-{{ $type->slug }}"--}}
+               {{--aria-selected="true"--}}
+               {{--onclick="">--}}
+                {{--{{ $type->title }}--}}
+            {{--</a>--}}
+        {{--</li>--}}
+        {{--@endforeach--}}
+    {{--</ul>--}}
 
-    <div class="tab-content" id="pills-tabContent">
-        @foreach($types as $content)
-            <div class="tab-pane fade {{ $loop->first ? 'show active' : '' }}" id="pills-{{ $content->slug }}" role="tabpanel" aria-labelledby="pills-{{ $content->slug }}-tab">
+    {{--<div class="tab-content" id="pills-tabContent">--}}
+        {{--@foreach($types as $content)--}}
+            {{--<div class="tab-pane fade {{ $loop->first ? 'show active' : '' }}" id="pills-{{ $content->slug }}" role="tabpanel" aria-labelledby="pills-{{ $content->slug }}-tab">--}}
 
 
-                <div class="container my-5">
-                    <div class="filter-section justify-content-center align-items-center">
-                        <button class="filter-button active" data-filter="{{ strtolower($content->slug) }}">All</button>
+                {{--<div class="container my-5">--}}
+                    {{--<div class="filter-section justify-content-center align-items-center">--}}
+                        {{--<button class="filter-button active" data-filter="{{ strtolower($content->slug) }}">All</button>--}}
 
-                        @foreach($content->categories as $category)
-                            <button class="filter-button"
-                                    data-filter="{{ strtolower($category->slug) }}">
-                                {{ $category->title }}
-                            </button>
-                        @endforeach
+                        {{--@foreach($content->categories as $category)--}}
+                            {{--<button class="filter-button"--}}
+                                    {{--data-filter="{{ strtolower($category->slug) }}">--}}
+                                {{--{{ $category->title }}--}}
+                            {{--</button>--}}
+                        {{--@endforeach--}}
 
-                    </div>
-                </div>
+                    {{--</div>--}}
+                {{--</div>--}}
 
                 {{--{{ dd($content->projects) }}--}}
 
-                <div class="container-fluid">
-                    <div class="card-columns portfolio-grid endless-pagination" data-route="{{ route('index') }}" data-next-page="{{ $projects->count() }}">
+                {{--<div class="container-fluid">--}}
+                    {{--<div class="card-columns portfolio-grid endless-pagination" data-route="{{ route('index') }}" data-next-page="{{ $projects->count() }}">--}}
 
-                        @include('index::front.boxes.projects', compact($projects))
+                        {{--@include('index::front.boxes.projects', compact($projects))--}}
 
-                    </div>
+                    {{--</div>--}}
 
-                    <div class="loader-container">
-                        <div id="loader"></div>
-                    </div>
+                    {{--<div class="loader-container">--}}
+                        {{--<div id="loader"></div>--}}
+                    {{--</div>--}}
 
-                    <div class="projects-loader-container">
-                        <div id="loader"></div>
-                    </div>
-                </div>
+                    {{--<div class="projects-loader-container">--}}
+                        {{--<div id="loader"></div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
 
 
 
-            </div>
-        @endforeach
-    </div>
+            {{--</div>--}}
+        {{--@endforeach--}}
+    {{--</div>--}}
 
 @endsection
